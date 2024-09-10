7d817a4d82f4bab6569c7a360167d976acdac5e5
Caue <caue_ftv@hotmail.com>
2024-09-09 21:35:42 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -10,23 +10,28 @@ console.log(mensagemlet)
 const mensagemconst = "Hello World!"
 console.log(mensagemconst)
 
-// arrays - usado pra listas e dados, usa os colchetes []
-let metas = ["alo", "caue"]
-console.log(metas[0] + " " + metas[1])
-
 //objetos - todos tem uma propriedade e valor
 let meta = {
     value: "ler um livro todo mês",
     address: 3,
-    checked: false,   
+    checked: true
     //uso do dado booleano (true or false)
 }
-console.log(meta.value)
-console.log(meta.checked)
 
+// arrays - usado pra listas e dados, usa os colchetes []
+let metas = [
+    meta,
+    {
+        value: "fazer exercícios diários",
+        checked: false
+    }
+]
+
+console.log(metas[0].value + ", " + metas[1].value),
+console.log(metas[0].checked + ", " + metas[1].checked),
+console.log(meta.value),
+console.log(meta.checked),
 // function - fora de um objeto, enquanto o método é usado dentro de um objeto (como mostrado acima)
 // arrow function - atribui a uma constante, usa o =>
 //const criarMeta = () => {} - Exemplo de arrow function
 function criarMeta() {}
-console.log(criarMeta)
-
